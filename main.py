190787d8b84b791f2c08d680d62e3c038340db13
Oleg <89175955650@mail.ru>
2023-09-17 14:40:04 +0300

    Next commit.

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import time
+
 import psutil
 
 
 def battery_charge():
-    pass
+    battery = psutil.sensors_battery()
+    battery_volume = int(battery.percent)
+    i = 0
+    for i in range(battery_volume):
+        time.sleep(0.05)
+        print(f"Count current charge volume: ....... {i}")
+        i += 1
+    print(f"The volume of battery charge is: {battery_volume}%.")
 
 
 battery_charge()
